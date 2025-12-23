6b0c85325817f871e3f64fddb2fc272ed1cf608f
Vinit Rokade <rvinit026@gmail.com>
2025-12-23 16:52:38 +0000

    Add encapsulation example with BankAccount

---
+++ Main.java
@@ -0,0 +1,44 @@
+class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance){
+        this.balance=initialBalance;
+    }
+
+    public void deposite(double amount){
+        if(amount>0){
+            balance+=amount;
+            System.out.println("Deposit Amount "+ amount +"RS" + " ** Current Balance "+balance+"RS" );
+        }else{
+            System.out.println("Enter Positive Value...!");
+        }
+    }
+
+    public void withdraw(double amount){
+        if(amount>0){
+            if(amount<=balance){
+                balance-=amount;
+                System.out.println("Withdraw Amount "+ amount +"RS" + " ** Current Balance "+balance+"RS" );
+            }else{
+                System.out.println("Please Enter Valid Amount....!");
+            }
+        }else{
+            System.out.println("Enter Positive Value");
+        }
+    }
+
+    public double getbalance(){
+        return balance;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+            BankAccount myAccount=new BankAccount(1000.0);
+
+            myAccount.deposite(300.0);
+            myAccount.withdraw(200.0);
+
+        System.out.println("*Final Balance* "+myAccount.getbalance()+"RS");
+    }
+}
